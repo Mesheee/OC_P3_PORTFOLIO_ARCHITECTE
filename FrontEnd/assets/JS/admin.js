7c597c8b949e5358f37d8c5c4ea0b7054120d54d
Nina Cliville <nina.cliville@gmail.com>
2023-03-30 23:09:59 +0200

    Update, fix bugs and add modal content

--- FrontEnd/assets/JS/admin.js
+++ FrontEnd/assets/JS/admin.js
@@ -1,36 +1,93 @@
-//OUVERTURE ET FERMETURE MODALES
+//MODIFICATIONS ADMIN MODE
+// Vérification de la connexion de l'utilisateur
+const token = localStorage.getItem('token');
+  if (token) {
+    // L'admninistrateur est connecté, affichage des éléments pour l'administrateur
+    const adminElements = document.querySelectorAll('#adminMode');
+    adminElements.forEach(adminMode => {
+      adminMode.style.display = 'flex';
+    });
+    // Modification du lien de la page HTML si l'administrateur est connecté
+    const loginLink = document.querySelector('#loginLink');
+    if (loginLink) {
+    loginLink.textContent = 'logout';
+    // Déconnexion de l'administrateur au clic sur "logout"
+    loginLink.addEventListener('click', e => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    window.location.href = 'login.html';
+    });
+    }
+    // Masquage des filtres si l'administrateur est connecté
+    const filtersElement = document.querySelector('#contener_filters');
+    if (filtersElement) {
+    filtersElement.style.display = 'none';
+    }
+  }
+
+//OUVERTURE ET FERMETURE DES MODALES
 const openModale = document.querySelector(".button_modif_modal");
 // Sélectionner tous les éléments avec l'ID "modal1"
 const modaleModeElements = document.querySelectorAll("#modal1");
-// Ajouter un événement au clic sur le bouton pour afficher les éléments modale
+// Ajouter un événement au clic sur le bouton pour afficher les éléments 
 openModale.addEventListener("click", () => modaleModeElements.forEach(e => e.style.display = "flex"));
-
 // Sélectionner l'icône pour fermer la modale
 const closeModale = document.querySelector(".fa-xmark");
 // Sélectionner tous les éléments avec les IDs "modal1" et "modal2"
 const modaleCloseElements = document.querySelectorAll("#modal1", "#modal2");
 // Ajouter un événement au clic sur l'icône pour cacher les éléments modale
 closeModale.addEventListener("click", () => modaleCloseElements.forEach(e => e.style.display = "none"));
-
 // Empêcher la propagation de l'événement de clic à l'intérieur de la modale
 const stopPropagation = (event) => event.stopPropagation();
 document.querySelector("#modal1").addEventListener("click", stopPropagation);
 document.querySelector("#modal2").addEventListener("click", stopPropagation);
-
-// Sélectionner l'icône pour retourner à la modale1
+// Sélectionner l'icône pour retourner à la modale 1
 const icon = document.querySelector(".fa-arrow-left");
-// Ajouter un événement au clic sur l'icône pour cacher la modale2 et afficher la modale1
+// Ajouter un événement au clic sur l'icône pour cacher la modale 2 et afficher la modale 1
 icon.addEventListener("click", () => {
 document.querySelector("#modal2").style.display = "none";
 document.querySelector("#modal1").style.display = "flex";
 });
-
 // Sélectionner le bouton pour ajouter des images
 const addImagesButton = document.querySelector(".button_add_images");
-// Ajouter un événement au clic sur le bouton pour afficher la modale2 et cacher la modale1
+// Ajouter un événement au clic sur le bouton pour afficher la modale 2 et cacher la modale 1
 addImagesButton.addEventListener("click", () => {
 const modaleModeElements = document.querySelectorAll("#modal2");
 modaleModeElements.forEach(e => e.style.display = "flex");
 const modaleCloseElements = document.querySelectorAll("#modal1");
 modaleCloseElements.forEach(e => e.style.display = "none");
 });
+
+
+//GENERER LE CONTENU DE LA MODALE 1
+//SUPPRESSION DES TRAVAUX
+function addModal(projects) {
+  // Récupération de l'élément du DOM qui accueillera la modale
+  const sectionModal = document.querySelector(".gallery_modal");
+  projects.forEach((article) => {
+    const figureModale = document.createElement("figure");
+    figureModale.classList.add("figureEdit");
+    const contenairImg = document.createElement("div");
+    contenairImg.classList.add("containerImg");
+    const imgModale = document.createElement("img");
+    imgModale.src = article.imageUrl;
+    imgModale.crossOrigin = "anonymous";
+    const editImg = document.createElement("figcaption");
+    editImg.innerText = "éditer";
+    const editIcon = document.createElement("i");
+    editIcon.classList.add("fa-solid", "fa-trash-can", "editIcon");
+    // Ajout d'un écouteur d'événement au clic sur l'icône de la corbeille
+    editIcon.addEventListener("click", () => {
+      figureModale.remove();
+    });
+    contenairImg.appendChild(imgModale);
+    figureModale.appendChild(contenairImg);
+    figureModale.appendChild(editImg);
+    figureModale.appendChild(editIcon);
+    sectionModal.appendChild(figureModale);
+  });
+}
+
+
+
+  
--- FrontEnd/assets/JS/index.js
+++ FrontEnd/assets/JS/index.js
@@ -1,118 +1,90 @@
-//ADMIN MODE//
-// Vérification de la connexion de l'utilisateur
-const token = localStorage.getItem('token');
+//RECUPERATION DES DONNEES API
+// Stocke les données récupérées dans une variable
+let projects;
+// Récupère les données des projets depuis l'API et les traite en JSON
+fetch("http://localhost:5678/api/works")
+  .then((response) => response.json())
+  .then((data) => {
+    // Stocke les données récupérées dans la variable projects
+    projects = data;
+    // Ajoute les projets à la section galerie
+    addProjects(projects);
+    // Ajoute les projets à la modale
+    addModal(projects);
+  })
+  .catch((error) => console.error(error));
 
-//Modification Admin Mode
-if (token) {
-  // L'utilisateur est connecté, affichage des éléments pour l'administrateur
-  const adminElements = document.querySelectorAll('#adminMode');
-  adminElements.forEach(adminMode => {
-    adminMode.style.display = 'flex';
-  });
 
-  // Modification du lien de la page HTML si l'utilisateur est connecté
-  const loginLink = document.querySelector('#loginLink');
-  if (loginLink) {
-  loginLink.textContent = 'logout';
-
-  // Déconnexion de l'utilisateur au clic sur "logout"
-  loginLink.addEventListener('click', e => {
-  e.preventDefault();
-  localStorage.removeItem('token');
-  window.location.href = 'login.html';
-  });
-  }
-
-  // Masquage de l'élément avec l'ID "filters" si l'utilisateur est connecté
-  const filtersElement = document.querySelector('#contener_filters');
-  if (filtersElement) {
-  filtersElement.style.display = 'none';
-  }
-}
 
 // WORKS // 
-// Cette fonction asynchrone récupère les projets à partir de l'API
-async function getProjects() {
-  // On effectue une requête fetch pour récupérer les données des projets
-  const response = await fetch("http://localhost:5678/api/works");
-  // On attend la résolution de la promesse renvoyée par la méthode json() de la réponse HTTP pour obtenir les données au format JSON
-  return await response.json();
-}
-
+// Ajoute les projets à la section galerie
 function addProjects(projects) {
-  // On sélectionne la section de la galerie dans le DOM
   const sectionGallery = document.querySelector(".gallery");
-  // On parcours tous les éléments du tableau
+  // Parcourt chaque projet dans le tableau projects
   for (const project of projects) {
-    // On extrait le titre et l'URL de l'image du projet 
-    const { title, imageUrl } = project;
-    // On crée un élément figure qui contiendra l'image et le titre du projet
+    // Destructure les propriétés id, title et imageUrl de l'objet project
+    const { id, title, imageUrl } = project;
+    // Crée un nouvel élément figure pour contenir l'image et le titre du projet
     const projectElement = document.createElement("figure");
-    // On crée un élément image et on lui attribue l'URL de l'image du projet
-    const imageElement = document.createElement("img");
+    projectElement.id = id;
+    // Crée un nouvel élément image avec la source définie sur l'URL de l'image du projet
+    const imageElement = new Image();
     imageElement.src = imageUrl;
-    // On crée un élément figcaption pour afficher le titre du projet
+    imageElement.crossOrigin = "anonymous";
+    // Crée un nouvel élément figcaption pour contenir le titre du projet
     const titleElement = document.createElement("figcaption");
-    titleElement.innerText = title;
-    // On ajoute l'image et le titre à l'élément figure créé précédemment
-    projectElement.append(imageElement, titleElement);
-    // On ajoute l'élément figure à la section de la galerie dans le DOM
+    titleElement.textContent = title;
+    // Ajoute les éléments image et titre à l'élément project
+    projectElement.appendChild(imageElement);
+    projectElement.appendChild(titleElement);
+    // Ajoute l'élément project à la section galerie
     sectionGallery.appendChild(projectElement);
   }
 }
 
-// Supprimer les projets affichés
+//FILTERS//
+// Supprime tous les éléments de la section galerie
 function deleteList() {
   document.querySelector(".gallery").textContent = "";
 }
 
-// On récupère les projets en utilisant la fonction "getProjects()" et on les affiche dans la galerie
-getProjects().then((projects) => {
+// Ajoute un bouton "Tous" pour afficher tous les projets
+const filters = document.querySelector("#filters");
+const btnAll = document.createElement("button");
+btnAll.innerHTML = "Tous";
+btnAll.id = "category";
+btnAll.classList.add("button_filters");
+filters.appendChild(btnAll);
+
+// Lorsque le bouton "Tous" est cliqué, supprime les projets actuels et affiche tous les projets
+btnAll.addEventListener("click", () => {
+  deleteList();
   addProjects(projects);
-  
-  // On crée un bouton pour afficher tous les projets dans la galerie
-  const filters = document.querySelector("#filters");
-  const btnAll = document.createElement("button");
-  btnAll.innerHTML = "Tous";
-  btnAll.id = "category";
-  btnAll.classList.add("button_filters");
-  filters.appendChild(btnAll);
+});
 
-  // On ajoute un gestionnaire d'événement pour le clic sur le bouton "Tous"
-  btnAll.addEventListener("click", () => {
-    // On supprime tous les éléments de la galerie
-    deleteList();
-    // On réaffiche tous les projets dans la galerie
-    addProjects(projects);
-  });
-
-// FILTERS //
- // On récupère les catégories à partir de l'API
+// Récupère les données de catégorie depuis l'API et ajoute des boutons pour chaque catégorie
 fetch("http://localhost:5678/api/categories")
-.then((response) => response.json())
-.then((categories) => {
-
-  // On boucle à travers toutes les catégories
-  for (const category of categories) {
-    // On crée un bouton pour chaque catégorie
-    const bouton = document.createElement("button");
-    bouton.classList.add("button_filters");
-    bouton.textContent = category.name;
-    // On ajoute un gestionnaire d'événement pour le clic sur le bouton de la catégorie
-    bouton.addEventListener("click", () => {
-    // On filtre les projets en fonction de la catégorie sélectionnée
-    const filteredProjects = projects.filter(
-    (project) => project.category.name === category.name
-    );
-    // On supprime tous les éléments de la galerie
-    deleteList();
-    // On affiche les projets filtrés dans la galerie
-    addProjects(filteredProjects);
-    });
-    // On ajoute le bouton de la catégorie à la liste des filtres
+  .then((response) => response.json())
+  .then((categories) => {
+    // Parcourt chaque catégorie dans le tableau categories
+    for (const category of categories) {
+      // Crée un nouvel élément bouton pour la catégorie
+      const bouton = document.createElement("button");
+      bouton.classList.add("button_filters");
+      bouton.textContent = category.name;
+      // Lorsque le bouton de la catégorie est cliqué, filtre les projets par catégorie et les affiche
+      bouton.addEventListener("click", () => {
+        const filteredProjects = projects.filter(
+          (project) => project.category.name === category.name
+        );
+        deleteList();
+        addProjects(filteredProjects);
+      });
     filters.appendChild(bouton);
   }
-});
-});
+})
+.catch((error) => console.error(error));
+
+
 
 
